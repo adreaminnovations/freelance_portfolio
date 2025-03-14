1b9a21d1a535bf936c9cda95cbb724456171a582
apurv-sttl <apurv.kantaria@silvertouch.com>
2025-03-14 13:04:05 +0530

    Updates

---
+++ precache-manifest.dd249b33961767a8e95da884e93738da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87e3b06df8b58b701b40e3299a9451b",
+    "url": "/freelance_portfolio/index.html"
+  },
+  {
+    "revision": "ecca183525afb6482996",
+    "url": "/freelance_portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aad367e33107ae23bbba",
+    "url": "/freelance_portfolio/static/js/2.fb27fa84.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/freelance_portfolio/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "ecca183525afb6482996",
+    "url": "/freelance_portfolio/static/js/main.f351c14d.chunk.js"
+  },
+  {
+    "revision": "97ed824e8260d8f7750b",
+    "url": "/freelance_portfolio/static/js/runtime~main.31f2876c.js"
+  }
+]);
